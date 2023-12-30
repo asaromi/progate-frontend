13795cb7f95cabf81bd1d09ea2dd63e89275ae1f
Asaromi <a.sabililromi@gmail.com>
2023-12-30 07:21:37 +0700

    first deploy

---
+++ assets/js/dom.js
@@ -0,0 +1,25 @@
+const result = document.getElementById('result')
+
+const button = document.querySelector('input[type="button"]')
+button.addEventListener('click', () => {
+  const value = document.querySelector('input[type="number"]').value
+
+  console.log(value)
+  if (!value || value === '0') {
+    result.innerHTML = 'Invalid value'
+    result.style.color = 'red'
+  } else {
+    result.innerHTML = ''
+    result.style.color = 'black'
+  }
+
+  if (value % 3 === 0 && value % 5 === 0) {
+    result.innerHTML += 'FizzBuzz!<br>'
+  } else if (value % 3 === 0) {
+    result.innerHTML += 'Fizz!<br>'
+  } else if (value % 5 === 0) {
+    result.innerHTML = 'Buzz!<br>'
+  } else {
+    result.innerHTML += value + '<br>'
+  }
+})
